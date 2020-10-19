f61e9d159eb7c6c342bbbe96fdbe271ab84d303e
theluanz <luanzuffo.lz@gmail.com>
2020-10-18 23:45:30 -0300

    Day 2 - NLW - Backend and Database

---
+++ backend/src/models/Orphanage.ts
@@ -0,0 +1,31 @@
+import {Entity, Column, PrimaryGeneratedColumn, OneToMany, JoinColumn} from 'typeorm';
+import Image from './Image';
+
+
+@Entity('orphanages')
+export default class Orphanage{
+    @PrimaryGeneratedColumn('increment')
+    id: number;
+    @Column()
+    name: string;
+    @Column()
+    latitude: number;
+    @Column()
+    longitude: number;
+    @Column()
+    about: string;
+    @Column()
+    instructions: string;
+    @Column()
+    opening_hours: string;
+    @Column()
+    open_on_wekeends: boolean;
+    
+    @OneToMany(()=> Image, image => image.orphanage, {
+      cascade: ['insert', 'update']
+    })
+
+    @JoinColumn ({name: 'orphanage_id'})
+    images: Image[];
+    
+}
